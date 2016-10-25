f18f9414b00ea2d9299b6c97b11a4ce1dcefe95a
Sergey Stolyrov <comp622012016@mail.ru>
2016-10-25 16:17:38 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-require_once 'Pbasa.php';
-$obj=new Pbasa();
-$obj->test();
+require_once 'ConcreteClass2.php';
+$class1 = new ConcreteClass1;
+$class1->printOut();
+echo $class1->prefixValue('FOO_') ."\n";
+
+$class2 = new ConcreteClass2;
+$class2->printOut();
+echo $class2->prefixValue('FOO_') ."\n";
 ?>
